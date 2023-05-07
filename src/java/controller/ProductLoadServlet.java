3104a9e6342af43137be986e5ffd2c4c0c9296b4
KennethFernando-CICS <kennethchristopher.fernando.cics@ust.edu.ph>
2023-05-07 18:56:11 +0800

    Added Listener, Product Loader, resources folder

--- src/java/controller/ProductLoadServlet.java
+++ src/java/controller/ProductLoadServlet.java
@@ -1,21 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author jemne
- */
-public class servletDummy extends HttpServlet {
+public class ProductLoadServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,20 +26,57 @@ public class servletDummy extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet servletDummy</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet servletDummy at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try{
+            PrintWriter out = response.getWriter();
+            
+            Connection conn = connectDB();
+
+            String query = "SELECT * FROM PRODUCTS FETCH FIRST 10 ROWS ONLY";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while(rs.next()){
+                System.out.println("Loaded Product ID:" + rs.getInt("PROD_ID"));
+                out.println("<div class=\"item-container\">\n" +
+                            "<div class=\"temp-image\">" +
+                            "<img class=\"item-img\" src=\"resources/images/" + rs.getString("IMAGE") + "\"/>" +
+                            "</div>" +
+                            "<h1 class=\"item-name\">" + rs.getString("Name") + "</h1>\n" +
+                            "<p class=\"price\">$" + rs.getDouble("Price") + "</p>\n" +
+                            "</div>"
+                );
+            }            
+            
+            //Close
+            rs.close();
+            stmt.close();
+            conn.close();
+          
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
+    public static Connection connectDB()
+    {
+        Connection conn = null;
+        try {
+            String driver = "org.apache.derby.jdbc.ClientDriver";
+            Class.forName(driver);
+            System.out.println("LOADED DRIVER: " + driver);
+            
+            String url = "jdbc:derby://localhost:1527/FapDB";
+            String username = "app";
+            String password = "app";
+            conn = DriverManager.getConnection(url, username, password);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+        return conn;
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
